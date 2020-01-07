a74a099e108777012fbf8bde781c667b46c60f10
Sahil Kapur <sahilkapur.a@gmail.com>
2020-01-07 03:50:25 -0800

    Linked airtable as backend

--- src/App.js
+++ src/App.js
@@ -23,6 +23,10 @@ class NavBar extends React.Component {
 }
 
 class ContentContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiKey: process.env.API_KEY, baseURL: "https://api.airtable.com/v0/appwaUv9OXdJ4UNpy", eventsData: [], newsData: [], upcomingData: [], spotlightData: {}, brotherData: {}, brotherName: "" }
+  }
   getData() {
     const data = {
       brotherName: "Sydney",
@@ -64,46 +68,127 @@ class ContentContainer extends React.Component {
         }
       ],
       events: [
-          {
-            "name": "Dinner with E-board at Panera",
-            "location": "Panera Bread",
-            "time": "6:00 PM",
-            "month": "jan",
-            "day": "13"
-          },
-          {
-            "name": "Back to School Olympics",
-            "location": "TBD",
-            "time": "9:00 PM",
-            "month": "jan",
-            "day": "28"
-          },
-          {
-            "name": "Volunteering at Animal Shelter",
-            "location": "Robert's Animal Shelter",
-            "time": "3:00 PM",
-            "month": "feb",
-            "day": "9"
-          }
-        ]
-      }
-      return data
+        {
+          "name": "Dinner with E-board at Panera",
+          "location": "Panera Bread",
+          "time": "6:00 PM",
+          "month": "jan",
+          "day": "13"
+        },
+        {
+          "name": "Back to School Olympics",
+          "location": "TBD",
+          "time": "9:00 PM",
+          "month": "jan",
+          "day": "28"
+        },
+        {
+          "name": "Volunteering at Animal Shelter",
+          "location": "Robert's Animal Shelter",
+          "time": "3:00 PM",
+          "month": "feb",
+          "day": "9"
+        }
+      ]
     }
+    return data
+  }
+
+  componentDidMount() {
+    this.getEventData();
+    this.getNewsData();
+    this.getUpcomingData();
+    this.getSpotlightData();
+    this.getBrotherData();
+  }
+
+  getBrotherData() {
+    //random variables at the end are used to sort events
+    const url = this.state.baseURL + "/brother_data?api_key=" + this.state.apiKey;
+    fetch(url).then(res => res.json())
+      .then((data) => {
+        console.log(data.records);
+        this.setState({ brotherData: data.records[0].fields, brotherName: data.records[0].fields["first_name"] });
+      });
+  }
+
+  getSpotlightData() {
+    //random variables at the end are used to sort events
+    const url = this.state.baseURL + "/spotlight?api_key=" + this.state.apiKey;
+    fetch(url).then(res => res.json())
+      .then((data) => {
+        console.log(data.records);
+        this.setState({ spotlightData: data.records[0].fields });
+      });
+  }
+
+  getUpcomingData() {
+    let upcomingData = [];
+    //random variables at the end are used to sort events
+    const url = this.state.baseURL + "/upcoming?api_key=" + this.state.apiKey;
+    fetch(url).then(res => res.json())
+      .then((data) => {
+        for (var i = 0; i < data.records.length; i++) {
+          upcomingData.push(data.records[i].fields);
+        }
+        this.setState({ upcomingData: upcomingData });
+      });
+  }
+
+  getNewsData() {
+    let newsData = [];
+    //random variables at the end are used to sort events
+    const url = this.state.baseURL + "/news?api_key=" + this.state.apiKey + "&sort%5B0%5D%5Bfield%5D=date_added";
+    fetch(url).then(res => res.json())
+      .then((data) => {
+        for (var i = 0; i < data.records.length; i++) {
+          newsData.push(data.records[i].fields);
+        }
+        this.setState({ newsData: newsData });
+      });
+  }
+
+  getEventData() {
+    let eventsData = [];
+    //random variables at the end are used to sort events
+    const url = this.state.baseURL + "/events?api_key=" + this.state.apiKey + "&sort%5B0%5D%5Bfield%5D=date";
+    fetch(url).then(res => res.json())
+      .then((data) => {
+        for (var i = 0; i < data.records.length; i++) {
+          eventsData.push(data.records[i].fields);
+        }
+        this.setState({ eventsData: eventsData });
+      });
+  }
+
+  getEventData() {
+    let eventsData = [];
+    //random variables at the end are used to sort events
+    const url = this.state.baseURL + "/events?api_key=" + this.state.apiKey + "&sort%5B0%5D%5Bfield%5D=date";
+    fetch(url).then(res => res.json())
+      .then((data) => {
+        for (var i = 0; i < data.records.length; i++) {
+          eventsData.push(data.records[i].fields);
+        }
+        this.setState({ eventsData: eventsData });
+      });
+  }
+
   render() {
     const data = this.getData();
     return (
       <div className="content-container">
-        <div class="column">
-          <ContentBox title={"Welcome, " + data.brotherName + " 👋"} height="6%" />
-          <ContentBox title="Chapter Attendance ❤️" data={data.attendance} content="Attendance" height="45%" />
-          <ContentBox title="Brother Spotlight 🤠" data={data.spotlight} content="Spotlight" height="41.2%" />
+        <div className="column">
+          <ContentBox title={"Welcome, " + this.state.brotherName + " 👋"} height="6%" />
+          <ContentBox title="Chapter Attendance ❤️" data={this.state.brotherData} content="Attendance" height="45%" />
+          <ContentBox title="Brother Spotlight 🤠" data={this.state.spotlightData} content="Spotlight" height="41.2%" />
         </div>
-        <div class="column">
-          <ContentBox title="Coming Up ⚡️" data={data.upcoming} content="Upcoming" height="44.7%" />
-          <ContentBox title="News Panel 📰" data={data.news} content="News" height="50%" />
+        <div className="column">
+          <ContentBox title="Coming Up ⚡️" data={this.state.upcomingData} content="Upcoming" height="44.7%" />
+          <ContentBox title="News Panel 📰" data={this.state.newsData} content="News" height="50%" />
         </div>
-        <div class="column">
-          <ContentBox title="Upcoming Events 📣" data={data.events} content="Event" height="67.7%" />
+        <div className="column">
+          <ContentBox title="Upcoming Events 📣" data={this.state.eventsData} content="Event" height="67.7%" />
           <ContentBox title="Drop us a Line 🤖" content="Slack" height="27%" />
         </div>
       </div>
@@ -118,15 +203,15 @@ class ContentBox extends React.Component {
 
   renderContent() {
     if (this.props.content == "Attendance") {
-      return <Attendance data={this.props.data}/>;
+      return <Attendance data={this.props.data} />;
     } else if (this.props.content == "Spotlight") {
-      return <Spotlight data={this.props.data}/>;
+      return <Spotlight data={this.props.data} />;
     } else if (this.props.content == "Upcoming" || this.props.content == "News") {
-      return <NewsList data={this.props.data}/>;
+      return <NewsList data={this.props.data} />;
     } else if (this.props.content == "Event") {
-      return <Events data={this.props.data}/>;
+      return <Events data={this.props.data} />;
     } else if (this.props.content == "Slack") {
-      return <Slack data={this.props.data}/>;
+      return <Slack data={this.props.data} />;
     }
   }
 
@@ -150,7 +235,7 @@ class Attendance extends React.Component {
     return (
       <div>
         <img src="attendance.png" className="attendance-graph" />
-        <p>We've seen you at 2 out of 5 chapters this semester.</p>
+        <p>We've seen you at {this.props.data.attendance} out of 5 chapters this semester.</p>
       </div>
     );
   }
@@ -160,11 +245,24 @@ class Spotlight extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  renderWhenReady() {
+    if (this.props.data.img != undefined) {
+      return (
+        <div>
+          <img className="spotlight-img" src={this.props.data.img[0].url} />
+          <p className="brother-description">Meet Akash Raju ('20): CEO of Glimpse and Founding Class of DMK</p>
+        </div>
+      )
+    }
+    return (
+      <div></div>
+    )
+  }
   render() {
     return (
       <div>
-        <img className="spotlight-img" src="akash.png" />
-        <p className="brother-description">Meet Akash Raju ('20): CEO of Glimpse and Founding Class of DMK</p>
+        {this.renderWhenReady()}
       </div>
     );
   }
@@ -179,7 +277,7 @@ class NewsList extends React.Component {
     var items = [];
     const data = this.props.data;
     for (var i = 0; i < data.length; i++) {
-      items.push(<NewsItem content={data[i].content} img={data[i].img} />);
+      items.push(<NewsItem content={data[i].headline} img={data[i].img[0].url} link={data[i].link} />);
     }
     return items;
   }
@@ -242,17 +340,22 @@ class NewsItem extends React.Component {
     super(props);
   }
 
+  handleClick() {
+    if (this.props.link != "#") {
+      window.open(this.props.link, "_blank")
+    }
+  }
+
   renderImage() {
-    console.log(this.props.img);
     if (this.props.img != undefined && this.props.img != "") {
       return (
         <div>
-          <div className="news-item-container">
+          <div className="news-item-container" onClick={() => this.handleClick()}>
             <div className="item-img-wrapper">
               <img className="item-img" src={this.props.img} />
             </div>
             <div className="item-text-wrapper">
-              <p>{this.props.content}</p>
+              <p className="item-content">{this.props.content}</p>
             </div>
           </div>
           <hr className="hr" width="70%" />
